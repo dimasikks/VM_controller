c89a9a86b85fc845157bbf00d8098ae217fbf861
dimasikks <email>
2026-01-24 23:55:16 +0300

    deleted funcs, inlines - v0.2

--- functions.py
+++
@@ -1,6 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-def get_start_keyboard() -> InlineKeyboardMarkup:
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="Status", callback_data="status")]
-    ])
