a032e6d150cf5c3520648f4fea5237b38b5374a7
dimasikks <email>
2026-01-27 23:03:39 +0300

    add img to status, logs, txt/img switch, html parse mode - v0.3

--- functions.py
+++ functions.py
@@ -9,23 +9,35 @@ from aiogram.filters import Command
 
 #=======# SERVER #=======#
 
-buttonTypes = {
+buttonTypesLogs = {
     "syslog": "TXT",
     "auth": "TXT",
     "dmesg": "TXT"
 }
 
+buttonTypesStatus = {
+    "mem": "TXT",
+    "cpu": "TXT",
+    "disks": "TXT"
+}
+
 async def tbut(callback: CallbackQuery):
     type_button = callback.data.split(":", 1)[1]
 
-    buttonTypes[type_button] = "IMG" if buttonTypes[type_button] == "TXT" else "TXT"
-        
-    await callback.answer()
-    await callback.message.edit_text(
-        text="Types:",
-        reply_markup=keyboards.get_logs_keyboard(buttonTypes)
-    )
-
+    if type_button in buttonTypesLogs:
+        buttonTypesLogs[type_button] = "IMG" if buttonTypesLogs[type_button] == "TXT" else "TXT"
+            
+        await callback.answer()
+        await callback.message.edit_reply_markup(
+            reply_markup=keyboards.get_logs_keyboard(buttonTypesLogs, log_prefix)
+        )
+    elif type_button in buttonTypesStatus:
+        buttonTypesStatus[type_button] = "IMG" if buttonTypesStatus[type_button] == "TXT" else "TXT"
+            
+        await callback.answer()
+        await callback.message.edit_reply_markup(
+            reply_markup=keyboards.get_status_keyboard(buttonTypesStatus, status_prefix)
+        )
 
 with open("configs/image.yml", "r", encoding="utf-8") as config:
     image_settings = yaml.safe_load(config)
@@ -116,92 +128,173 @@ async def start(message: Message):
     ip = await command_shell("hostname -I")
 
     await message.answer(
-        f"USER: {message.from_user.first_name}\n\nVM: {hostname}\nIP: {ip}",
+        f"<b>USER: </b>{message.from_user.first_name}\n\n"
+        f"<b>VM: </b>{hostname}\n"
+        f"<b>IP: </b>{ip}\n",
+        parse_mode="HTML",
         reply_markup=keyboards.get_start_keyboard()
     )
 
 #=======# STATUS #=======#
 
+status_prefix = "status_"
+status_lines = 10
+statuses = [
+    f"{status_prefix}mem",
+    f"{status_prefix}cpu",
+    f"{status_prefix}disks"
+]
+scan_disks = [
+    "/dev/*"
+]
+ps_limiter = 10
+
 async def status(message: Message):
+    hostname = await command_shell("hostname")
+    la = await command_shell(f"uptime | grep -Eo '[0-9],[0-9]{{2}}.*'")
+
+    mem_total = await command_shell(r"""free -m | awk 'NR==2 {printf "%.2f\n",$2/1024}'""")
+    mem_used = await command_shell(r"""free -m | awk 'NR==2 {printf "%.2f\n",$3/1024}'""")
+    mem_avail = await command_shell(r"""free -m | awk 'NR==2 {printf "%.2f\n",$7/1024}'""")
+
+    cpus = await command_shell(r"""top -bn1 | grep -Eo "[0-9].*" | awk 'NR==3 {print}' | column | sed 's/,  /  /g'""")
+    uptime = await command_shell(f"uptime -p")
+
+    counter = 1
+    priv_disks = await command_shell(f"df -h | awk 'NR==1 {{print}}'") + "\n"
+    for disk in scan_disks:
+        output = await command_shell(f"df -h | grep -E '^{disk}'")
+
+        if counter == len(scan_disks):
+            priv_disks += f"{output}"
+        else:
+            priv_disks += f"{output}\n"        
+
     await message.answer(
-        "Metrics:",
-        reply_markup=keyboards.get_status_keyboard()
+        f"<b>STATUS OF {hostname}</b>\n"
+        f"{uptime}\n"
+        f"\n<b><u>MEMORY STATUS</u></b>\n"
+        f"<b>Total:</b> {mem_total} Gb\n"
+        f"<b>Used:</b> {mem_used} Gb\n"
+        f"<b>Available:</b> {mem_avail} Gb\n"
+        f"\n<b><u>CPU STATUS</u></b>\n"
+        f"<b>Load Average:</b> {la}\n"
+        f"<b>CPUs:</b> {cpus}\n"
+        f"\n<b><u>DISKS STATUS</u></b>\n"
+        f"<b>Stats for choosen disks:</b>\n"
+        f"<pre>{priv_disks}</pre>",
+        parse_mode="HTML",
+        reply_markup=keyboards.get_status_keyboard(buttonTypesStatus, status_prefix)
     )
 
-async def status_mem(callback: CallbackQuery):
+async def status_definer(callback: CallbackQuery):
     await callback.answer()
 
-    mem = await command_shell("free -h")
+    if callback.data == f"{status_prefix}mem":
+        if buttonTypesStatus["mem"] == "TXT":
 
-    await callback.message.answer(
-        f"{mem}"
-    )
+            mem_top = await command_shell(f"ps -eo 'pid ppid user %mem %cpu command' --sort=-%mem | awk '{{print substr($0,1,50)}}' | head -n {ps_limiter}")
 
-async def status_cpu(callback: CallbackQuery):
-    await callback.answer()
+            await callback.message.answer(
+                f"<b>TOP {ps_limiter} MEMORY PROCESSES</b>\n\n"
+                f"<pre>{mem_top}</pre>",
+                parse_mode="HTML"
+                )
+        else:
+            path = await command_image(f"ps -eo 'pid ppid user %mem %cpu command' --sort=-%mem | head -n {ps_limiter}")
 
-    cpu = await command_shell("uptime")
+            img = FSInputFile(path)
+            await callback.message.answer_photo(
+                img,
+                caption=f"<b>Last {ps_limiter} lines of ps sorted by memory usage</b>",
+                parse_mode="HTML"
+            )
+    elif callback.data == f"{status_prefix}cpu":
+        if buttonTypesStatus["cpu"] == "TXT":
+            cpu_top = await command_shell(f"ps -eo 'pid ppid user %mem %cpu command' --sort=-%cpu | awk '{{print substr($0,1,50)}}' | head -n {ps_limiter}")
 
-    await callback.message.answer(
-        f"{cpu}"
-    )
+            await callback.message.answer(
+                f"<b>TOP {ps_limiter} CPU PROCESSES</b>\n\n"
+                f"<pre>{cpu_top}</pre>",
+                parse_mode="HTML"
+                )
+        else:
+            path = await command_image(f"ps -eo 'pid ppid user %mem %cpu command' --sort=-%cpu | head -n {ps_limiter}")
 
-async def status_disks(callback: CallbackQuery):
-    await callback.answer()
+            img = FSInputFile(path)
+            await callback.message.answer_photo(
+                img,
+                caption=f"<b>Last {ps_limiter} lines of ps sorted by CPU usage</b>",
+                parse_mode="HTML"
+            )
+    else:
+        if buttonTypesStatus["disks"] == "TXT":
+            df_stat = await command_shell("df -h")
+            lsblk = await command_shell("lsblk | grep -v 'loop'")
 
-    disks = await command_shell("df -h")
+            await callback.message.answer(
+                f"<b>DF</b>\n"
+                f"<pre>{df_stat}</pre>\n"
+                f"\n<b>LSBLK</b>\n"
+                f"<pre>{lsblk}</pre>",
+                parse_mode="HTML"
+                )
+        else:
+            path = await command_image("echo -e '|=============| DF |=============|\n'; df -h; echo -e '\n|=============| LSBLK |=============|\n'; lsblk | grep -v 'loop'")
 
-    await callback.message.answer(
-        f"<code>{disks}</code>",
-        parse_mode="HTML"
-    )
+            img = FSInputFile(path)
+            await callback.message.answer_photo(
+                img,
+                caption=f"<b>Disks information</b>",
+                parse_mode="HTML"
+            )
 
 #=======# LOGS #=======#
 
+log_prefix = "logs_"
+log_lines = 10
 log_paths = {
-    "logs_syslog": "/var/log/syslog",
-    "logs_auth": "/var/log/auth.log",
-    "logs_dmesg": "/var/log/dmesg",
+    f"{log_prefix}syslog": "/var/log/syslog",
+    f"{log_prefix}auth": "/var/log/auth.log",
+    f"{log_prefix}dmesg": "/var/log/dmesg"
 }
-log_lines = 15
+log_commands = {
+    f"{log_prefix}syslog": f"sudo tail -n {log_lines} {log_paths[f'{log_prefix}syslog']}",
+    f"{log_prefix}auth": f"sudo tail -n {log_lines} {log_paths[f'{log_prefix}auth']}",
+    f"{log_prefix}dmesg": f"sudo tail -n {log_lines} {log_paths[f'{log_prefix}dmesg']}"
+}
 
 async def logs(message: Message):
     await message.answer(
         "Types:",
-        reply_markup=keyboards.get_logs_keyboard(buttonTypes)
+        reply_markup=keyboards.get_logs_keyboard(buttonTypesLogs, log_prefix)
     )
 
-async def logs_syslog(callback: CallbackQuery):
+async def logs_definer(callback: CallbackQuery):
     await callback.answer()
 
-    path = await command_image(f"sudo tail -n {log_lines} {log_paths['logs_syslog']}")
+    for log_type, path in log_paths.items():
+        if log_type == callback.data:
+            command = log_commands[log_type]
 
-    img = FSInputFile(path)
-    await callback.message.answer_photo(img)
+            if buttonTypesLogs[log_type[len(log_prefix):]] == "TXT":
 
-    # await callback.message.answer(
-    #     f"<b>Last {log_lines} of {log_paths['logs_syslog']}:</b>\n\n"
-    #     f"<pre>{output}</pre>",
-    #     parse_mode="HTML"
-    # )
+                output = await command_shell(command)
 
-async def logs_dmesg(callback: CallbackQuery):
-    await callback.answer()
+                await callback.message.answer(
+                f"<b>Last {log_lines} of {log_paths[log_type]}</b>\n\n"
+                f"<pre>{output}</pre>",
+                parse_mode="HTML"
+                )
+            else:
+                path = await command_image(command)
 
-    cpu = await command_shell("uptime")
-
-    await callback.message.answer(
-        f"{cpu}"
-    )
-
-async def logs_dmesg(callback: CallbackQuery):
-    await callback.answer()
-
-    cpu = await command_shell("uptime")
-
-    await callback.message.answer(
-        f"{cpu}"
-    )
+                img = FSInputFile(path)
+                await callback.message.answer_photo(
+                    img,
+                    caption=f"<b>Last {log_lines} of {log_paths[log_type]}</b>",
+                    parse_mode="HTML"
+                )
 
 #=======# FALLBACK #=======#
 
@@ -219,8 +312,9 @@ def register_handlers(dp: Dispatcher):
     # dp.message.register(systmed, F.text == "Systemd")
     dp.message.register(fallback)
 
-    dp.callback_query.register(status_mem, F.data == "status_mem")
-    dp.callback_query.register(status_cpu, F.data == "status_cpu")
-    dp.callback_query.register(status_disks, F.data == "status_disks")
+    for flog_type_data, path in log_paths.items():
+        dp.callback_query.register(logs_definer, F.data == flog_type_data)
 
-    dp.callback_query.register(logs_syslog, F.data == "logs_syslog")
+    for fstatus_type_data in statuses:
+        dp.callback_query.register(status_definer, F.data == fstatus_type_data)
+        
--- keyboards.py
+++ keyboards.py
@@ -11,16 +11,16 @@ def get_start_keyboard() -> InlineKeyboardMarkup:
         one_time_keyboard=False    
     )
 
-def get_status_keyboard() -> InlineKeyboardMarkup:
+def get_status_keyboard(buttonTypes: dict, prefix: str) -> InlineKeyboardMarkup:
         return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="MEM", callback_data="status_mem")],
-        [InlineKeyboardButton(text="CPU", callback_data="status_cpu")],
-        [InlineKeyboardButton(text="DISK", callback_data="status_disks")]
+        [InlineKeyboardButton(text="MEM", callback_data=f"{prefix}mem"), InlineKeyboardButton(text=buttonTypes["mem"], callback_data="tbut:mem")],
+        [InlineKeyboardButton(text="CPU", callback_data=f"{prefix}cpu"), InlineKeyboardButton(text=buttonTypes["cpu"], callback_data="tbut:cpu")],
+        [InlineKeyboardButton(text="DISK", callback_data=f"{prefix}disks"), InlineKeyboardButton(text=buttonTypes["disks"], callback_data="tbut:disks")]
     ])
 
-def get_logs_keyboard(buttonTypes: dict) -> InlineKeyboardMarkup:
+def get_logs_keyboard(buttonTypes: dict, prefix: str) -> InlineKeyboardMarkup:
         return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="syslog", callback_data="logs_syslog"), InlineKeyboardButton(text=buttonTypes["syslog"], callback_data="tbut:syslog")],
-        [InlineKeyboardButton(text="auth", callback_data="logs_auth"), InlineKeyboardButton(text=buttonTypes["auth"], callback_data="tbut:auth")],
-        [InlineKeyboardButton(text="dmesg", callback_data="logs_dmesg"), InlineKeyboardButton(text=buttonTypes["dmesg"], callback_data="tbut:dmesg")]
+        [InlineKeyboardButton(text="syslog", callback_data=f"{prefix}syslog"), InlineKeyboardButton(text=buttonTypes["syslog"], callback_data="tbut:syslog")],
+        [InlineKeyboardButton(text="auth", callback_data=f"{prefix}auth"), InlineKeyboardButton(text=buttonTypes["auth"], callback_data="tbut:auth")],
+        [InlineKeyboardButton(text="dmesg", callback_data=f"{prefix}dmesg"), InlineKeyboardButton(text=buttonTypes["dmesg"], callback_data="tbut:dmesg")]
     ])
